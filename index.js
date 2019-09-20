413d5bdbb5936617afc17d5d2acd8827f7b580c2
Renato Gama <renatoargh@gmail.com>
2019-09-20 23:27:39 +0200

    Allows for loading multiple lambdas at the same time

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 #!/usr/bin/env node
 
 const express = require('express')
+const morgan = require('morgan')
+const getPort = require('get-port')
 const toLambda = require('./expressToLambda')
 const getMockedContext = require('./getMockedContext')
 const app = express()
 const stage = express.Router()
-const {NODE_ENV: stagePath} = process.env
+const {NODE_ENV: stagePath = 'dev'} = process.env
 
+const fs = require('fs')
 const path = require('path')
+const pkg = require('./package.json')
 const pwd = process.env.PWD
-const indexPath = path.join(pwd, 'index.js')
-const {handler} = require(indexPath)
 
-const port = process.argv[2] || 8080
+const isMulti = process.argv.includes('--multi')
+
+let handlersPaths = [path.join(pwd, 'index.js')]
+
+if (isMulti) {
+  handlersPaths = fs
+    .readdirSync(pwd, {withFileTypes: true})
+    .filter(d => d.isDirectory() && d.name !== 'node_modeules')
+    .map(d => path.join(pwd, `/${d.name}`, 'index.js'))
+}
 
 const safe = (middleware) => 
   (req, res, next) => Promise.resolve(middleware(req, res, next)).catch(next)
 
+app.use(morgan('tiny'))
 app.use(express.json())
 app.use(`/${stagePath}`, stage)
 
-handler.expressCompatibility.forEach((route) => {
-  stage[route.method](route.path, safe(async (req, res) => {
-    const event = toLambda(req, route.resource)
-    console.log(req.params)
-
-    const context = getMockedContext()
-
-    const response = await handler(event, context)
-
-    res.status(response.statusCode)
-    res.set(response.headers)
-    res.send(response.body)
-  }))
+app.all('/', (req, res) => {
+  res.redirect(`/${stagePath}`)
 })
 
-app.listen(port, () => {
-  console.log('try your lambda/api gateway integration at:')
-  console.log(`> http://localhost:${port}/${stagePath}`)
+console.log(`> ${pkg.name} - ${pkg.description}\n`)
+
+handlersPaths.forEach(handlerPath => {
+  console.log('>', path.dirname(handlerPath.replace(pwd, '')).replace(/^\//, ''))
+
+  try {
+    const {handler} = require(handlerPath)
+    handler.expressCompatibility.forEach((route) => {
+      console.log(`  ${route.method.toUpperCase()} ${route.path}`)
+
+      stage[route.method](route.path, safe(async (req, res) => {
+        const event = toLambda(req, route.resource)
+        const context = getMockedContext()
+        const response = await handler(event, context)
+
+        res.status(response.statusCode)
+        res.set(response.headers)
+        res.send(response.body)
+      }))
+    })
+  } catch (err) {
+    console.log('> error:', err.message, '(skipping)')
+  }
+
+  console.log()
 })
+
+async function main() {
+  const port = await getPort({
+    port: getPort.makeRange(3000, 8080)
+  })
+
+  app.listen(port, () => {
+    console.log(`> api available at:`)
+    console.log(`  http://localhost:${port}/${stagePath}\n`)
+  })
+}
+
+main()
